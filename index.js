9e5b77f2b787c3e452c5f2949046c29e92fa387c
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-01-06 18:34:03 -0700

    style header

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ home.style.height = `${height}px`;
 
 let about = document.getElementById('about');
 about.style.height = `${height}px`;
+
+
+let contact = document.getElementById('contact');
+let contactHeight = window.innerHeight - 50;
+contact.style.height = `${contactHeight}px`;

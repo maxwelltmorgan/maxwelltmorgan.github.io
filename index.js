fb444eb63213b26162d5d3d87fa507902ce678a7
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-01-15 00:54:51 -0700

    recaptcha script

--- index.js
+++ index.js
@@ -26,18 +26,3 @@ function scrollFinished() {
 
 var form = document.getElementById('#1');
 honey(form);
-
-function submitUserForm() {
-    var response = grecaptcha.getResponse();
-    console.log(response.length);
-    if(response.length == 0) {
-        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
-        return false;
-    }
-    return true;
-}
-
-function verifyCaptcha() {
-    console.log('verified');
-    document.getElementById('g-recaptcha-error').innerHTML = '';
-}
